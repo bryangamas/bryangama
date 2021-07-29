f415b3bb153276b55c416b85397aabaf95d62c75
bgamas <bryangaso15@gmail.com>
2021-07-28 19:28:58 -0500

    feat: adding links to courses

--- next.config.js
+++ next.config.js
@@ -4,4 +4,26 @@ module.exports = {
     defaultLocale: "en",
   },
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/c/react",
+        destination:
+          "https://platzi.com/p/bgamas/curso/2118-react-hooks/diploma/detalle/",
+        permanent: true,
+      },
+      {
+        source: "/c/next",
+        destination:
+          "https://platzi.com/p/bgamas/curso/1991-next/diploma/detalle/",
+        permanent: true,
+      },
+      {
+        source: "/c/figma",
+        destination:
+          "https://platzi.com/p/bgamas/curso/1961-figma/diploma/detalle/",
+        permanent: true,
+      },
+    ];
+  },
 };
